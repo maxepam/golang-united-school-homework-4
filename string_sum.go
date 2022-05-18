2dcd86dde11c2570cfec2b65ffd83d3b9b801b75
Max Gor <m.hornostaiev@myfeelix.de>
2022-05-19 01:54:40 +0300

    added solution

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +26,66 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	input = strings.TrimSpace(input)
+	if input == "" {
+		err = fmt.Errorf("Bad string %w", errorEmptyInput)
+		return "", err
+	}
+	input = strings.Replace(input, " ", "", -1)
+
+	isFirstNegative := strings.HasPrefix(input, "-")
+
+	if isFirstNegative {
+		input = input[1:]
+	}
+
+	opsPositive := strings.Split(input, "+")
+	opsNegative := strings.Split(input, "-")
+
+	if len(opsNegative) != 2 && len(opsPositive) != 2 {
+		err = fmt.Errorf("Bad string %w", errorNotTwoOperands)
+		return "", err
+	}
+
+	var opFirstStr, opSecStr string
+	if len(opsNegative) == 2 {
+		opFirstStr = opsNegative[0]
+		opSecStr = "-" + opsNegative[1]
+	 } else {
+		opFirstStr = opsPositive[0]
+		opSecStr = opsPositive[1]
+	 }
+
+	 if (isFirstNegative) {
+		opFirstStr = "-" + opFirstStr 
+	 }
+
+	var opFirst, opSec int 
+
+	opFirst, err = strconv.Atoi(opFirstStr)
+	if err != nil {
+		err = fmt.Errorf("First operand is not right %w", err)
+		return "", err
+	}
+
+	opSec, err = strconv.Atoi(opSecStr)
+	if err != nil {
+		err = fmt.Errorf("Second operand is not right %w", err)
+		return "", err
+	}
+
+	sum := opFirst + opSec
+	output = strconv.Itoa(sum)
+
+	return output, nil
 }
+
+// func main() {
+// 	str := "-3-5"
+// 	fmt.Println(str)
+// 	o, err := StringSum(str)
+// 	if err == nil {
+// 		fmt.Println(o)
+// 	}
+// 	fmt.Println(err)
+// }
